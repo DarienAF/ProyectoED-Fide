1ca0980f11bdd01cc18cb453ce40b50d4327a9a5
DarienAF <123607154+DarienAF@users.noreply.github.com>
2023-08-06 17:32:24 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Overcooked-Fide
-        //7/5/2023 Prueba clone y pull
-        //PUSH
+
     }
     
 }
